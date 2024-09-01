c67f1244ddd8dbd529e06b8e63b2cd8843f3e6d2
thdud <thgml1444@naver.com>
2024-09-01 13:25:06 +0900

    Add : prettier.config.js 설정 - 기본적인 세팅만 설정해둠 - arrowParens: 화살표 함수의 인자를 괄호로 묶음 - bracketSpacing: 객체,배열의 여러 줄 표시할 때 {},[] 앞뒤에 공백넣기 - embeddedLanguageFormatting: 코드블록 서식 지정 자동으로 - jsxSingleQuote: JSX속성값 작은따옴표로 통일 - printWidth: 한 줄에 출력할 문자 수 120으로 지정 - semi: 문장 끝에 세미콜론 추가 - singleQuote: 문자열 사용시 작은따옴표 통일 - tabWidth: 탭을 2개의 공백으로 설정 - trailingComma: 배열,객체의 마지막 요소 뒤에 쉼표 설정정

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+const config = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  jsxSingleQuote: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
