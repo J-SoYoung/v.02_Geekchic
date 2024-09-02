23bba93a985ff7ba2551f56cbece7db2d674587a
thdud <thgml1444@naver.com>
2024-09-02 16:47:47 +0900

    Add : 메인 App컴포넌트에 전체 layout스타일 지정 - index.css : root의 프로젝트 전체 배경 색상 설정 - index.css : App컴포넌트에 classname 연결하여 기본 스타일 width, background-color 등 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import BottomNav from './components/BottomNav';
 
 function App() {
   return (
-    <>
+    <div className='App min-h-screen w-[600px]'>
       <Outlet />
-    </>
+      <BottomNav/>
+    </div>
   );
 }
 
