93b9e247c742ae03dd8fc28ba1d44853934537e3
thdud <thgml1444@naver.com>
2024-09-03 11:00:44 +0900

    Add : 로그인 UI 및 firebase db연결 - firebase에서 제공하는 소셜-구글로그인 기능을 통해 로그인 - 위 로그인 시 fireabase에서 해당 유저가 있는지 확인 후 있으면 로그인을 유지하며 서비스 메인 페이지로 이동 -firebase에 유저가 없는 경우 유저 초기화 데이터와 함께 firebase에 저장 후 서비스 메인 페이지로 이동

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react';
-// image, icon
-import { FcGoogle } from "react-icons/fc";
+import { FcGoogle } from 'react-icons/fc';
+import { signInWithGoogle, signOutFromGoogle } from '@apis/auth';
 import Loginlogo from '@assets/rootImage/loginLogo.png';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-  const [localUser, setLocalUser] = useState(false);
+  const navigate = useNavigate();
 
-  const onClickLogin = () => {};
-  const onClickLogout = () => {};
+  const [userData, setUserData] = useState(null);
+  const [loginError, setLoginError] = useState(false);
+
+  const onClickLogin = async () => {
+    
+    try {
+      const user = await signInWithGoogle();
+      setUserData(user);
+      navigate('/used')
+    } catch (error) {
+      console.error('Logout failed:', error);
+      setLoginError(true);
+    }
+  };
+
+  const onClickLogout = async () => {
+    try {
+      await signOutFromGoogle();
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
 
   return (
     <main>
@@ -17,11 +38,12 @@ export const Login = () => {
       <section className='px-20'>
         <button
           className='w-full h-[50px] flex items-center justify-center mb-2 rounded-md bg-white hover:brightness-90 border border-black'
-          onClick={!localUser ? onClickLogin : onClickLogout}
+          onClick={userData ? onClickLogout : onClickLogin}
         >
-          <FcGoogle className='mr-1 w-6 h-6'/>
-          {!localUser ? 'Google로 로그인' : 'Google 로그아웃'}
+          <FcGoogle className='mr-1 w-6 h-6' />
+          {userData ? 'Google 로그아웃' : 'Google로 로그인'}
         </button>
+        {loginError && <p className='font-bold text-red-600'>로그인을 다시 시도해주세요</p>}
       </section>
     </main>
   );
