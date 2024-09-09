4d716be740f64e0080f0264efe01d4108d1ccd81
thdud <thgml1444@naver.com>
2024-09-09 13:13:43 +0900

    Add : 판매목록 UI 생성 및 데이터 연결 - UsedProductCard 컴포넌트 생성 -> 중고메인과 공통으로 사용 - 중고메인 api와 판매목록 가져온s api 공통화 할 수있을 것 같음 -> 진행예정

---
+++ src/components/UsedProductCard.tsx
@@ -0,0 +1,65 @@
+import { Link } from 'react-router-dom';
+
+interface UsedProductCardProps {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  quantity: number;
+  isSoldOut: boolean;
+  showSellerInfo?: boolean; // 판매목록에서는 판매량 정보 보여주기
+  sellerId?: string;
+  sellsQuantity?: number; // 판매 수량 (판매목록에서 사용)
+}
+
+// ⭕컴포넌트 나누기 : object-cover 이미지 검색되는 것 avatar, user, 정보 
+export const UsedProductCard = ({
+  id,
+  name,
+  price,
+  image,
+  quantity,
+  isSoldOut,
+  showSellerInfo = false,
+  sellsQuantity,
+}: UsedProductCardProps) => {
+  return (
+    <section>
+      <Link
+        to={`/used/detail/${id}`}
+        className={`rounded-lg cursor-pointer relative ${isSoldOut && 'opacity-50'}`}
+      >
+        {image ? (
+          <img
+            className='w-full h-48 object-cover rounded-md mb-2'
+            src={image}
+            alt={name}
+          />
+        ) : (
+          <img
+            src='/default-image.png'
+            className='w-full h-48 object-cover rounded-md mb-2 border'
+          />
+        )}
+        <div className='px-2'>
+          <div className='flex'>
+            <h2 className='text-lg font-bold mr-1'>{name}</h2>
+          </div>
+          <div className='flex items-center justify-between'>
+            <p>{price.toLocaleString()}원</p>
+            {isSoldOut ? (
+              <p className='text-red-500'>( 품절 )</p>
+            ) : showSellerInfo ? (
+              <div className='mt-2 text-gray-500'>
+                <p>재고: {quantity}개</p>
+                <p>판매: {sellsQuantity}개</p>
+              </div>
+            ) : (
+              <p>{`( ${quantity}개 남음 )`}</p>
+            )}
+          </div>
+        </div>
+      </Link>
+    </section>
+  );
+};
---
+++ src/pages/sellsList/SellsList.tsx
@@ -0,0 +1,76 @@
+import { getMyPageInfo } from '@/_apis/apis';
+import { SellsItemType } from '@/_typesBundle/productType';
+import { Layout } from '@/components/Layout';
+import { UsedProductCard } from '@/components/UsedProductCard';
+
+
+import { useQuery } from '@tanstack/react-query';
+import { Link, useParams } from 'react-router-dom';
+
+export const SellsList = () => {
+  const { userId } = useParams<string>();
+
+  const {
+    data: sellsItem,
+    isPending,
+    isError,
+  } = useQuery<SellsItemType[]>({
+    queryKey: ['sellsItem'],
+    queryFn: async () => await getMyPageInfo(userId as string),
+  });
+
+  // ⭕ 로티이미지 추가 : 에러 페이지 데이터 새로고침 해주세요
+  if (isError) {
+    return (
+      <div className='border h-40 p-2 text-center'>
+        <p>데이터를 가져오는 동안 문제가 발생했습니다</p>
+        <button
+          className='cursor-pointer hover:font-bold'
+          onClick={() => window.location.reload()}
+        >
+          GeekChic MY 판매목록 페이지 새로고침
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <Layout title='판매목록'>
+      {isPending ? (
+        <p>로딩중</p>
+      ) : (
+        <div className='text-left'>
+          <div className=' text-m text-gray-600 m-8 mb-4 pb-4 border-b'>
+            <span className='font-bold'>전체 {sellsItem?.length}</span>
+          </div>
+          <div className='p-8 pt-4 grid grid-cols-2 gap-4 mb-24'>
+            {sellsItem?.length === 0 ? (
+              <div>
+                판매중인 상품이 없습니다.
+                <p>
+                  <Link to='/usedHome'>중고 홈 구경하기</Link>
+                </p>
+              </div>
+            ) : (
+              sellsItem?.map((item) => {
+                return (
+                  <UsedProductCard
+                    key={item.usedProductId}
+                    id={item.usedProductId}
+                    name={item.productName}
+                    price={item.price}
+                    image={item.image}
+                    quantity={item.quantity}
+                    isSoldOut={item.quantity < 1}
+                    showSellerInfo={true}
+                    sellsQuantity={item.sellsQuantity}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+};
