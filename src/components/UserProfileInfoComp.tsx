b3ebcbfbe57c980b7b5e9366dbad3a72c8a93cbc
thdud <thgml1444@naver.com>
2024-09-10 15:20:46 +0900

    Add : 유저 프로필 컴포넌트

---
+++ src/components/UserProfileInfoComp.tsx
@@ -0,0 +1,27 @@
+interface UserInofCompProps {
+  avatar: string;
+  username: string;
+  address: string;
+}
+
+export const UserProfileInfoComp = ({
+  avatar,
+  username,
+  address,
+}: UserInofCompProps) => {
+  return (
+    <div className='flex pb-6'>
+      <div className='w-12 h-12 bg-gray-200 rounded-full'>
+        <img
+          src={avatar}
+          alt='유저'
+          className='w-full h-full object-cover rounded-full border'
+        />
+      </div>
+      <div className='ml-4'>
+        <p className='text-lg font-semibold'>{username}</p>
+        <p className='text-sm text-gray-500'>{address}</p>
+      </div>
+    </div>
+  );
+};
