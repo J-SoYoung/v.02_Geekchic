3aa77b2d877943676e56a595ce608cb5b5e96281
thdud <thgml1444@naver.com>
2024-09-25 12:43:59 +0900

    Update : 구매내역 타입 생성, 구매내역 페이지에서 제품 컴포넌트 생성 및 데이터 렌더링 함

--- src/components/MyProductCard.tsx
+++ src/components/MyProductCard.tsx
@@ -1,37 +1,39 @@
 interface MyProductCardPropsType {
-  image: string;
+  createdAt: string[];
+  productImage: string;
   productName: string;
   price: number;
-  quantity?: number; // 장바구니 페이지에서만 필요
+  quantity: number; 
   isCarts?: boolean; // 장바구니 페이지인지 여부
   size: string;
 }
 
 export const MyProductCard = ({
-  image,
+  createdAt,
+  productImage,
   productName,
-  price,
-  quantity,
-  isCarts = false,
   size,
+  quantity,
+  price,
 }: MyProductCardPropsType) => {
   return (
-    <div className='flex items-center justify-between border rounded-md p-3 mb-2'>
-      <img
-        src={image}
-        alt={productName}
-        className='w-16 h-16 object-cover rounded-md'
-      />
-      <div className='flex-1 ml-4'>
-        <h2 className='text-sm font-bold'>{productName}</h2>
-        <p className='text-sm text-gray-500'>{size}</p>
-        <p className='text-base font-bold'>{price.toLocaleString()}원</p>
-      </div>
-      {isCarts && (
-        <div className='flex items-center'>
-          <span className='text-sm text-gray-500'>x {quantity}</span>
+    <div className='flex flex-col items-start p-4 rounded-md border-b'>
+      <p className='text-m text-gray-500 mb-2'>{createdAt[0]}</p>
+      <div className='flex '>
+        <img
+          src={productImage}
+          alt={productName}
+          className='w-16 h-16 mr-4 object-cover rounded-md'
+        />
+        <div className='flex flex-col items-start'>
+          <h2 className='text-sm font-bold'>{productName}</h2>
+          <p>
+            <span className='text-sm text-gray-500'>{size}</span> |
+            <span className='text-sm text-gray-500'> {quantity}개</span>
+          </p>
+          <p className='text-base font-bold'>{price.toLocaleString()}원</p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
