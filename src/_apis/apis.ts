1fc5161fcaf1b70c4b484fc32ed728de5f456174
thdud <thgml1444@naver.com>
2024-09-29 15:52:16 +0900

    Add : wishList 데이터 가져오기 - wishList의 데이터는 productId이므로, 이를 가져와 product database에서 데이터를 가져와 배열로 반환

--- src/_apis/apis.ts
+++ src/_apis/apis.ts
@@ -1,6 +1,6 @@
 import { ProductType } from '@/_typesBundle';
 import { database } from './firebase';
-import { get, push, ref, remove, set, update } from 'firebase/database';
+import { get, ref, remove, set, update } from 'firebase/database';
 
 export const uploadProducts = async (newProducts: ProductType) => {
   try {
@@ -38,6 +38,35 @@ export const removeWishData = async ({
     return remove(wishListRef);
   } catch (error) {
     console.error('관심물품 삭제 에러', error);
+  }
+};
+
+export const getWishList = async ({ userId }: { userId: string }) => {
+  try {
+    const wishDataSnapshot = await get(ref(database, `wishList/${userId}`));
+    if (wishDataSnapshot.exists()) {
+      const wishProductIds = wishDataSnapshot.val();
+
+      const productPromises = Object.keys(wishProductIds).map(
+        async (productId) => {
+          const productSnapshot = await get(
+            ref(database, `products/${productId}`),
+          );
+          if (productSnapshot.exists()) {
+            return productSnapshot.val();
+          } else {
+            return [];
+          }
+        },
+      );
+      return await Promise.all(productPromises);
+    } else {
+      console.log('위시리스트가 존재하지 않습니다.');
+      return [];
+    }
+  } catch (error) {
+    console.error('관심물품 리스트 가져오기 에러', error);
+    return [];
   }
 };
 
