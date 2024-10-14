95ef5a3e20f30e214b359368798b3df8dfa347ad
thdud <thgml1444@naver.com>
2024-10-14 13:07:45 +0900

    Refactoring : react.memo로 검색바 컴포넌트를 메모이제이션함 - Home컴포넌트에서 카테고리 등 리렌더링이 되어도 검색과 관련이 없으면 리렌더링이 되지 않도록 함 - 검색바는 검색과 관련된 state가 업데이트 될 때 리렌더링 된다

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,47 +1,44 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 import { searchProducts, SearchResult } from '@/_apis';
+import { validateSearchQuery } from '@/_utils';
 
 interface SearchBarProps {
   url: string;
-  setSearchResult: React.Dispatch<
-    React.SetStateAction<SearchResult[]>
-  >;
+  setSearchResult: React.Dispatch<React.SetStateAction<SearchResult[]>>;
   setIsSearching: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const SearchBar = ({
-  url,
-  setSearchResult,
-  setIsSearching,
-}: SearchBarProps) => {
-  
-  const [searchQuery, setSearchQuery] = useState('');
+export const SearchBar = memo(
+  ({ url, setSearchResult, setIsSearching }: SearchBarProps) => {
+    const [searchQuery, setSearchQuery] = useState('');
 
-  // ⭕mutation으로 검색기능 구현
-  const onClickButtonSearch = async () => {
-    const result = await searchProducts({ searchQuery, url });
-    setSearchResult(result);
-    setIsSearching(true);
-  };
+    // ⭕mutation으로 검색기능 구현
+    const onClickButtonSearch = async () => {
+      if (!validateSearchQuery(searchQuery)) return;
+      const result = await searchProducts({ searchQuery, url });
+      setSearchResult(result);
+      setIsSearching(true);
+    };
 
-  return (
-    <section className='w-full h-[50px] px-4 mb-8 flex items-center justify-between text-xl bg-[#EEE] rounded-lg'>
-      <input
-        className='w-full outline-none placeholder-gray-500 bg-[#EEE]'
-        type='text'
-        placeholder='상품검색'
-        maxLength={50}
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') onClickButtonSearch();
-        }}
-      />
-      <button onClick={onClickButtonSearch} className='bg-[#EEE] h-full'>
-        <BsSearch />
-      </button>
-    </section>
-  );
-};
+    return (
+      <section className='w-full h-[50px] px-4 mb-8 flex items-center justify-between text-xl bg-[#EEE] rounded-lg'>
+        <input
+          className='w-full outline-none placeholder-gray-500 bg-[#EEE]'
+          type='text'
+          placeholder='상품검색'
+          maxLength={50}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') onClickButtonSearch();
+          }}
+        />
+        <button onClick={onClickButtonSearch} className='bg-[#EEE] h-full'>
+          <BsSearch />
+        </button>
+      </section>
+    );
+  },
+);
