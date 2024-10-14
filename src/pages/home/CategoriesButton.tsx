d795fb8c47b1ecfc221eec3c0a2186093413803f
thdud <thgml1444@naver.com>
2024-10-14 13:09:07 +0900

    Refactoring : 카테고리 버튼도 react.memo를 통해 메모이제이션 함 - Home컴포넌트에서 카테고리와 상관없는 업데이트로 리렌더링 되지 않음

--- src/pages/home/CategoriesButton.tsx
+++ src/pages/home/CategoriesButton.tsx
@@ -1,18 +1,15 @@
-import { CategoriesButtonProps } from "@/_typesBundle";
+import { memo } from 'react';
+import { CategoriesButtonProps } from '@/_typesBundle';
 
-export const CategoriesButton = ({
-  title,
-  value,
-  activeTab,
-  setActiveTab,
-}: CategoriesButtonProps) => {
-
-  return (
-    <button
-      onClick={() => setActiveTab(value)}
-      className={`m-3 py-1 px-2 hover:bg-gray-100 ${activeTab === value ? 'text-[#b66cf7] font-bold' : 'text-gray-500'}`}
-    >
-      {title}
-    </button>
-  );
-};
+export const CategoriesButton = memo(
+  ({ title, value, activeTab, setActiveTab }: CategoriesButtonProps) => {
+    return (
+      <button
+        onClick={() => setActiveTab(value)}
+        className={`m-3 py-1 px-2 hover:bg-gray-100 ${activeTab === value ? 'text-[#b66cf7] font-bold' : 'text-gray-500'}`}
+      >
+        {title}
+      </button>
+    );
+  },
+);
