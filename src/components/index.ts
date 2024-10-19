251fbbcea04c75ebe9beb1ea48bccb53ef768904
thdud <thgml1444@naver.com>
2024-10-19 13:37:43 +0900

    Updage : export하는 컴포넌트를 묶어서 처리, 새롭게 컴포넌트 생성한 것도 index에 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,25 +6,17 @@ import { UserProfileInfoComp } from './UserProfileInfoComp';
 import { BottomNav } from './BottomNav';
 import { SellerMark } from './SellerMark';
 import { ErrorPageReload } from './ErrorPageReload';
-import { LoginButton, BasicButton } from './button';
-import { Comment, CommentButton, CommentInput, CommentsList } from './comment';
-import { SearchBar, SearchList } from './searchBar';
+export * from './button';
+export * from './comment';
+export * from './searchBar';
 
 export {
   Layout,
   MyProductCard,
   UsedProductCard,
   UserProfileInfoComp,
-  LoginButton,
-  BasicButton,
   BottomNav,
   SellerMark,
   ErrorPageReload,
   LoadingSpinner,
-  Comment,
-  CommentButton,
-  CommentInput,
-  CommentsList,
-  SearchBar,
-  SearchList,
 };
