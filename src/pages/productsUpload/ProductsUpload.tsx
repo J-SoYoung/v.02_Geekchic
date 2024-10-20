f9d63b27780ddc2f80f19fded6fc538e917c278c
thdud <thgml1444@naver.com>
2024-10-20 13:26:26 +0900

    Refactoring : 업로드 컴포넌트에서 useProductUpload hooks을 사용하게 되면서 비즈니스 로직과 UI로직을 분리하였다 - useProductUpload의 반환값으로는 isLoading 상태값과 onClickUploadProducts함수이다. - loading상태값을 사용해 업로드시의 상태를 사용자 화면에 반영할 수 있게 되고, 업로드 버튼에 hook이 반환한 함수를 연결하여 해당 버튼을 클릭해 hooks이 동작할 수 있도록 하였다. - 이로써 180줄이나 되는 페이지 코드를 120줄로 줄일 수 있게 됐다.

--- src/pages/productsUpload/ProductsUpload.tsx
+++ src/pages/productsUpload/ProductsUpload.tsx
@@ -1,82 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import { ProductType } from '@/_typesBundle';
 import { FormInput, FormRadio, UploadImage } from '../usedProductsUpload';
 import { BasicButton, Layout, LoadingSpinner } from '@/components';
 import { initProduct } from '@/_example';
-import { utcToKoreaTimes, validateProductData } from '@/_utils';
-import { useMutation } from '@tanstack/react-query';
-import { uploadCloudImagesArray, uploadProducts } from '@/_apis';
 import { useInput } from '@/hooks/useInput';
+import { useProductUpload } from './useProductUpload';
 
 export const ProductsUpload = () => {
   const navigate = useNavigate();
-  const { values: products, handleChange, reset } = useInput<ProductType>(initProduct);
-
   const [previewImages, setPreviewImages] = useState<string[]>([]);
   const [uploadImages, setUploadImages] = useState<File[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-
+ 
+  const { values: products, handleChange, reset } = useInput<ProductType>(initProduct);
+  const { isLoading, onClickUploadProducts } = useProductUpload(products,uploadImages)
 
   const onClickMoveUsedMain = () => {
     if (confirm('제품 업로드를 취소하겠습니까?')) {
       reset()
       navigate('/');
-    }
-  };
-
-  const imageUploadMutation = useMutation({
-    mutationFn: async (uploadImages: File[]) => {
-      return uploadCloudImagesArray(uploadImages);
-    },
-  });
-
-  const productUploadMutation = useMutation({
-    mutationFn: async (newProducts: ProductType) => {
-      await uploadProducts(newProducts);
-    },
-    onSuccess: () => {
-      navigate('/');
-    },
-    onError: (error) => {
-      console.log('제품 업로드 에러', error);
-      alert('업로드 중 에러가 발생했습니다.');
-    },
-    onSettled: () => {
-      setIsLoading(false);
-    },
-  });
-
-  const onClickUploadProducts = () => {
-    setIsLoading(true);
-    if (!validateProductData(products)) {
-      setIsLoading(false);
-      return alert('모든 필수 필드를 입력해주세요');
-    }
-    const id = uuidv4();
-    const createdAt = utcToKoreaTimes();
-
-    let newProducts: ProductType = {
-      ...products,
-      id,
-      createdAt,
-    };
-
-    if (uploadImages) {
-      imageUploadMutation.mutate(uploadImages, {
-        onSuccess: (cloudImage) => {
-          newProducts = { ...newProducts, images: cloudImage };
-          productUploadMutation.mutate(newProducts);
-        },
-        onError: (error) => {
-          console.log('이미지 업로드 에러', error);
-          alert('이미지 업로드 중 에러가 발생했습니다. 다시 시도해주세요 ');
-          setIsLoading(false);
-        },
-      });
     }
   };
 
